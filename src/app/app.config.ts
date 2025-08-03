74e5904b200771249f1c32971775242e6b9e6765
orelby <1364809+orelby@users.noreply.github.com>
2025-08-03 21:36:01 +0300

    Improve scroll position on navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,39 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
 
-import { routes } from './app.routes';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withInMemoryScrolling,
+  withViewTransitions
+} from '@angular/router';
+
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withViewTransitions(),
+      withInMemoryScrolling({
+        // - Fixes scroll position not changing on navigation in some browsers (mobile)
+        // - Seems to eliminate all USPs (Unidentified Pre-View Transition Scroll Phenomena)
+        // 
+        // TODO: Prevent scroll position restoration sometimes?
+        // (https://github.com/angular/angular/issues/26744)
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      })
+    ),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch())
   ]

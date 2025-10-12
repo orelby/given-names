283db11de8bf257093cc76ebcd911aaeba13f3da
orelby <1364809+orelby@users.noreply.github.com>
2025-10-12 23:36:46 +0300

    Fix navbar breakpoints
    Fixed #5

--- src/app/layout/header.ts
+++ src/app/layout/header.ts
@@ -24,7 +24,6 @@ import { END_YEAR } from '@shared/models/year-periods';
     MatButtonModule, MatIcon, MatNavList, MatListItem,
   ],
   host: {
-    'class': 'page page-md page-centered',
     '[class.has-drawer-open]': 'this.$isDrawerOpen()',
   },
   templateUrl: './header.html',
